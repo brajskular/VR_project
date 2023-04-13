f56c9c1f2809e3d4699711e86432c761df1e52d2
SeanJKelly <seanjosephkelly14@hotmail.com>
2023-04-13 14:09:17 -0500

    Merged

--- Assets/Scripts/moveBody.cs
+++ Assets/Scripts/moveBody.cs
@@ -8,62 +8,69 @@ using TMPro;
 
 public class moveBody : MonoBehaviour
 {
-public bool isTriggered;
+    public bool isTriggered;
 
     //public int score = 0;
     //public TMP_Text scoreText;
     public Transform player;
-    public float max_speed = 0.1f;
+    public float max_speed = 0.02f;
     public float speed = 0.0f;
     private bool accelerate = false;
     private bool decelerate = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-     isTriggered = false;   
-     accelerate = false;
-     decelerate = false;
+        isTriggered = false;
+        accelerate = false;
+        decelerate = false;
         XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
         grabInteractable.activated.AddListener(moveB);
         grabInteractable.deactivated.AddListener(stopB);
     }
-public void moveB(ActivateEventArgs args){
-isTriggered = true;
-accelerate = true;
-decelerate = false;
-}
+    public void moveB(ActivateEventArgs args)
+    {
+        isTriggered = true;
+        accelerate = true;
+        decelerate = false;
+    }
 
-public void stopB(DeactivateEventArgs args){
-isTriggered = false;
-decelerate = true;
-accelerate = false;
-}
-    
+    public void stopB(DeactivateEventArgs args)
+    {
+        isTriggered = false;
+        decelerate = true;
+        accelerate = false;
+    }
+
     // Update is called once per frame
     void Update()
-    {  
-        if(accelerate){
+    {
+        if (accelerate)
+        {
             speed += 0.005f;
-            if(speed >= max_speed){
+            if (speed >= max_speed)
+            {
                 accelerate = false;
                 speed = max_speed;
             }
-        } else if(decelerate){
+        }
+        else if (decelerate)
+        {
             speed -= 0.02f;
-            if(speed <= 0.0f){
+            if (speed <= 0.0f)
+            {
                 decelerate = false;
                 speed = 0;
             }
         }
-        
+
         // if(isTriggered){
         //score = int.Parse(scoreText.text);
         //score++;
         //scoreText.text = score.ToString();
-            Vector3 direction = transform.forward;
-            //transform.position += direction * speed;
-            player.position += direction * speed;
-            // }
+        Vector3 direction = transform.forward;
+        //transform.position += direction * speed;
+        player.position += direction * speed;
+        // }
     }
 }
